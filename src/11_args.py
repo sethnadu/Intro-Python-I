df6183b030fef6f62d7d363636828605b7fc6b9d
Seth Nadu <seth.nadu@gmail.com>
2019-12-02 18:23:43 -0500

    finished small error;

--- src/11_args.py
+++ src/11_args.py
@@ -58,9 +58,9 @@ print(f3(8))     # Should print 9
 
 # YOUR CODE HERE
 
-def f4(**args):
-    for key, value in args.items():
-        print("key:", key, "value:", value)
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print("key:", key + ",", "value:", value)
    
 f4(foo="bar", baz=12)    
 
@@ -81,4 +81,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(*d)
+f4(**d)
