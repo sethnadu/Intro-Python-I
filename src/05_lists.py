6def3c4f2c849d8c94b519cae4ea3994e8bab0ba
Seth Nadu <seth.nadu@gmail.com>
2019-12-02 14:24:45 -0500

    priting, lists, tuples, slices

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,31 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE 
-print(x)
+print(x.append(4))
+print("Problem 1:", x)
+
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE 
-print(x)
+print(x.extend(y))
+print("Problem 2:", x)
+
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE 
-print(x)
+print(x.remove(8))
+print("Problem 3:", x)
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE 
-print(x)
+print(x.insert(5, 99))
+print("Problem 4:", x)
 
 # Print the length of list x
 # YOUR CODE HERE 
+print("Length of list x", len(x))
 
 # Print all the values in x multiplied by 1000
 # YOUR CODE HERE
+x_multiplied = [i * 1000 for i in x]
+print("X list values * by 1000", x_multiplied)
